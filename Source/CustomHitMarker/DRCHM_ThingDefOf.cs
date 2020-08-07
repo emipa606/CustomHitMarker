0e58333857275b6a023e143d4734cae0090d4c61
emipa606 <emil.offermo@litet.net>
2020-08-07 21:38:31 +0200

    First publish

---
+++ Source/CustomHitMarker/DRCHM_ThingDefOf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RimWorld;
+using Verse;
+
+namespace CustomHitMarker
+{
+	// Token: 0x02000003 RID: 3
+	[DefOf]
+	public static class DRCHM_ThingDefOf
+	{
+		// Token: 0x04000001 RID: 1
+		public static ThingDef DRCHM_Hitmarker;
+
+		// Token: 0x04000002 RID: 2
+		public static SoundDef DRCHM_Hitmarker_Sound;
+	}
+}
