fdf92a0a6ec486c6285414ddb06a8a77673ae064
Vincent Wong <vincentwwy168@gmail.com>
2025-07-20 17:42:24 +0800

    Fix double-marker issue

--- Source/CustomHitMarker/Thing_TakeDamage.cs
+++ Source/CustomHitMarker/Thing_TakeDamage.cs
@@ -1,8 +1,6 @@
-using System;
 using HarmonyLib;
 using RimWorld;
 using Verse;
-using Verse.Sound;
 
 namespace CustomHitMarker;
 
@@ -15,6 +13,11 @@ public static class Thing_TakeDamage
         {
             return;
         }
+
+        // when a pawn is about to take damage, this method is called
+        // this method is called before the game checks for e.g. shields and armor
+        // then, the game calls DamageWorker.Apply(...), which may apply special damage effects to the pawn (e.g. to stun the pawn)
+        // as such, no need to spawn hit markers at DamageWorker.Apply(...)
 
         if (dinfo.Def == DamageDefOf.Flame || dinfo.Def == DamageDefOf.Mining ||
             dinfo.Def == DamageDefOf.Deterioration || dinfo.Def == DamageDefOf.Rotting)
